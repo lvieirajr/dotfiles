12e69c64d57d91e23cac23d02b546e5114fdede3
Luis Vieira <lvieira@lvieira.com>
2020-12-02 14:26:39 -0800

    conflict

---
+++ setup.sh
@@ -0,0 +1,34 @@
+rsync --exclude ".git/" \
+    --exclude ".idea/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		--exclude "brew.sh" \
+		--exclude "bin/" \
+		--exclude "setup.sh" \
+		-avh --no-perms . ~;
+
+brew update
+brew upgrade
+
+brew bundle
+
+brew cleanup
+
+source .macos
+
+rm -rf ~/init/ ~/Brewfile*
+rm -rf ./Brewfile.lock.json
+
+BREW_PREFIX=$(brew --prefix)
+
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+source ~/.bash_profile;
