863e8481f4e8b17b69ea5cb78c8c0892a47dc9b5
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2024-05-07 01:48:45 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,44 +1,49 @@
 #!/bin/bash
 
+# Set shell options
 set -eu
 shopt -s dotglob
 
-
+# Constants
 HOMEBREW="/opt/homebrew"
 OH_MY_ZSH="$HOME/.oh-my-zsh"
 POWERLEVEL10K="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 WORKSPACE="$HOME/Workspace"
 DOTFILES="$WORKSPACE/dotfiles"
 
-
+# Setup start
 echo -e "Setting up Luis Vieira's dotfiles.\n"
 cd $HOME
 
-
+# HomeBrew
 if [ ! -d $HOMEBREW ]; then
     echo -e "\nInstalling HomeBrew...\n"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo -e "\nFinished installing HomeBrew.\n"
 fi
 
+# Oh My ZSH
 if [ ! -d $OH_MY_ZSH ]; then
     echo -e "\nInstalling Oh my zsh...\n"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     echo -e "\nFinished installing Oh my zsh.\n"
 fi
 
+# Powerlevel10k
 if [ ! -d $POWERLEVEL10K ]; then
     echo -e "\nInstalling Powerlevel10k...\n"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     echo -e "\nFinished installing Powerlevel10k.\n"
 fi
 
+# Workspace
 if [ ! -d $WORKSPACE ]; then
     echo -e "\nCreating Workspace directory...\n"
     mkdir $WORKSPACE
     echo -e "\nFinished creating Workspace directory.\n"
 fi
 
+# Dotfiles
 echo -e "\nCloning dotfiles...\n"
 git clone -f --depth=1 https://github.com/lvieirajr/dotfiles.git $DOTFILES
 echo -e "\nFinished cloning dotfiles.\n"
@@ -47,14 +52,17 @@ echo -e "\nCopying dotfiles to home directory...\n"
 rsync -av --force --exclude=.git --exclude=README.md --exclude=setup.sh $DOTFILES/* $HOME
 echo -e "\nFinished copying dotfiles to home directory.\n"
 
+# MacOS defaults
 read -p "User friendly computer name: " computer_name
 read -p "Computer's host name: " host_name
 read -p "Computer's Net BIOS name (15 char limit): " net_bios_name
 
+# Brewfile
 echo -e "\nRunning Brewfile...\n"
 brew bundle install
 echo -e "\nFinished running Brewfile.\n"
 
+# Clean up
 echo -e "\nCleaning up...\n"
 rm -rf $DOTFILES
 echo -e "\nFinished cleaning up.\n"
